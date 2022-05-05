055167e64bad631b19b26b3d16bb126f45dae105
Victor Moreira <victor.moreira@bit.com.br>
2022-05-05 17:16:51 -0300

    Variables and routes

--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -2,12 +2,16 @@ import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from '../components/components.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    ComponentsModule
+    ComponentsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   declarations: [
     HomeComponent,
