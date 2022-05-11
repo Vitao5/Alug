f78d8c1cb0bba709aca138f4e5f335aec1e99e32
iloveyellowcakes <daniellerukia@hotmail.com>
2022-05-11 20:58:40 -0300

    adicionando footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { CardService } from 'src/app/services/card.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from 'src/app/services/card';
+import { News } from './news';
 
 @Component({
   selector: 'footer',
@@ -6,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  @Input() card!: Card[]
+  @Input() news: any
+
+  constructor(
+    private cardService: CardService
+  ) { }
 
   ngOnInit() {
+    this.getCard();
+  }
+
+  async getCard() {
+    const data$ = await lastValueFrom(this.cardService.getCard());
+    // this.card = data$
+
+
+    this.news = {
+      firstNew: data$.slice(-1)[0],
+      secondNew: data$.slice(-2)[0],
+      lastNew: data$.slice(-3)[0]
+    }
+    console.log(data$.slice(-2)[0]);
+
+
   }
 
 }
---
+++ src/app/components/footer/news.ts
@@ -0,0 +1,5 @@
+export interface News {
+  teste1: any,
+  teste2: any,
+  teste3: any
+}
